b83a94e1836dffe1ee709978ed9ca309f5c9eda6
Rakibul Yeasin <ryeasin03@gmail.com>
2019-09-20 14:35:11 +0600

    Updated :mag:

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ def img_resize(image_location):
 
     if src_width > src_height:
         # dst_width, dst_height = max_height, max_width
-        # dst_width, dst_height = 2063, 1080
-        dst_width, dst_height = src_width - 1080, src_height - 1080
+        dst_width, dst_height = 2160, 1112
     else:
         dst_width, dst_height = max_width, max_height
 
