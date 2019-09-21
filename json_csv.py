a6f3b6e7bc720c34804750a7d235da2c10ea5336
Rakibul Yeasin <rytotul@gmail.com>
2019-09-21 16:41:27 +0600

    Scripts Updated! :mag:

--- json_csv.py
+++ json_csv.py
@@ -3,7 +3,7 @@ import csv
 # Keys to get exact info from NASA-Dict
 data_locations = ["title", "url", "date", "copyright", "hdurl", "explanation", "media_type", "service_version"]
 
-d_data = [{
+data = [{
     "copyright": "Giorgia HoferCortina Astronomical Association",
     "date": "2019-09-16",
     "explanation": "What are those colorful rings around the Moon? A corona. Rings like this will sometimes appear when the Moon is seen through thin clouds. The effect is created by the quantum mechanical diffraction of light around individual, similarly-sized water droplets in an intervening but mostly-transparent cloud. Since light of different colors has different wavelengths, each color diffracts differently. Lunar Coronae are one of the few  quantum mechanical color effects that can be easily seen with the unaided eye.  The featured lunar corona was captured around full Moon above Turin, Italy in 2014.  Similar coronae that form around the Sun are usually harder to see because of the Sun's great brightness.",
@@ -48,11 +48,7 @@ d_data = [{
     "url": "https://apod.nasa.gov/apod/image/1909/Tulip_CropRobertEder1024.jpg"
 }]
 
-i = 1
-for data in d_data:
-    with open('nasa_data.csv', 'r+') as nasa:
-        print(i)
-        i += 1
-        d_data = csv.DictWriter(nasa, restval="-", fieldnames=data_locations, delimiter='@')
-        d_data.writeheader()
-        d_data.writerow(data)
+with open('nasa_data.csv', 'r+') as nasa:
+    d_data = csv.DictWriter(nasa, restval="-", fieldnames=data_locations)
+    d_data.writeheader()
+    d_data.writerows(data)
