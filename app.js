b4674ac404566db0ba99e411a096b9f18125fde3
Rakibul Yeasin <rytotul@gmail.com>
2020-08-28 21:32:00 +0600

    Added Image Download Feature :fire:

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const Instagram = require('./Instagram')
 const FileCookieStore = require('tough-cookie-filestore2');
 const fetch = require('node-fetch');
+const fs = require('fs');
+const request = require('request');
 
 // const username = "astronomyimages";
 // const password = "";
@@ -15,6 +17,14 @@ const client = new Instagram({ username, password, cookieStore });
 // Nasa API
 const api = 'https://api.nasa.gov/planetary/apod/?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo';
 
+// Download Image
+const download = function (uri, filename, callback) {
+    request.head(uri, function (err, res, body) {
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+};
+
+// Fetch the Image details from NASA api
 const getData = async url => {
     try {
         let res = await fetch(url);
@@ -25,6 +35,8 @@ const getData = async url => {
     }
 }
 
+
+// Do the task
 getData(api).then(res => {
     var data = res;
     let credit = '\n\nThis is an auto-generated and auto-published post.The pictures and captions are taken from the NASA API https://api.nasa.gov/. This System is developed by @drreygur';
@@ -61,4 +73,11 @@ getData(api).then(res => {
     }
 
     post(data, tags);
+
+    let imgExt = data.hdurl.split('.');
+    imgExt = imgExt[imgExt.length - 1];
+
+    download(data.hdurl, `./Images/${data.date}.${imgExt}`, () => {
+        console.log('[+] Done!');
+    });
 })
