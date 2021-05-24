2f437396d554b7e0e484d0de67bf03bc510be5d7
Rakibul Yeasin <rytotul@gmail.com>
2021-05-24 16:51:02 +0600

    Added Local Download feature for future use :mag:

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// const Instagram = require('instagram-web-api');
 const Instagram = require('./Instagram')
 const FileCookieStore = require('tough-cookie-filestore2');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const path = require('path');
 const request = require('request');
 
-// const username = "astronomyimages";
-// const password = "";
-
+// Collect Username and Password from Github's variables as arguments
 const username = process.argv[1];
 const password = process.argv[2];
 
-const cookieStore = new FileCookieStore('./cookies.json');
+// Saving Cookies for skipping login next time
+const cookieStore = new FileCookieStore(path.join('./', 'cookies.json');
 const client = new Instagram({ username, password, cookieStore });
 
 // Nasa API
@@ -20,7 +19,8 @@ const api = 'https://api.nasa.gov/planetary/apod/?api_key=NNKOjkoul8n1CH18TWA9gw
 // Download Image
 const download = function (uri, filename, callback) {
     request.head(uri, function (err, res, body) {
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        let location = path.join('./', 'images',filename)
+        request(uri).pipe(fs.createWriteStream(location)).on('close', callback);
     });
 };
 
@@ -40,25 +40,8 @@ const getData = async url => {
 getData(api).then(res => {
     var data = res;
     let credit = '\n\nThis is an auto-generated and auto-published post.The pictures and captions are taken from the NASA API https://api.nasa.gov/. This System is developed by @drreygur';
-    /* let tags = [
-                "#dailyastronomy", "#astronomy",
-                "#space", "#nasa", "#universe", "#astrophotography", "#science", "#cosmos", "#moon", "#stars",
-                "#galaxy", "#astrophysics", "#nightsky", "#photography", "#physics", "#milkyway", "#spacex", "#cosmology",
-                "#astro", "#earth", "#astronomia", "#sky", "#nature", "#telescope", "#astronaut", "#nightphotography",
-                "#solarsystem", "#night", "#planets", "#mars", "#bhfyp"
-               ]; */
-    
     let mTags = "#astronomy #space #nasa #universe #astrophotography #science #cosmos #moon #stars #galaxy #astrophysics #nightsky #photography #physics #milkyway #spacex #cosmology #astro #earth #astronomia #sky #nature #telescope #astronaut #nightphotography #solarsystem #night #planets #mars #bhfyp"
     
-    
-//     var mTags = (tags, n) => {
-//       tag = "";
-//         for(var i=0; i < n; i++) {
-//             tag += tags[Math.floor(Math.random() * tags.length)] + " ";
-//       }
-//         return tag;
-//     }
-
     var post = async (data, mTags) => {
         console.log(data);
         await client.login();
@@ -82,12 +65,8 @@ getData(api).then(res => {
         console.log(res);
     }
 
+    // Post to Insta
     post(data, mTags);
-
-    // let imgExt = data.hdurl.split('.');
-    // imgExt = imgExt[imgExt.length - 1];
-
-    // download(data.hdurl, `./Images/${data.date}.${imgExt}`, () => {
-    //     console.log('[+] Done!');
-    // });
+    // Save the image locally
+    download(data.hdurl, data.hdurl.split('/').reverse()[0], (res) => res);
 })
