8f9c9887a279677f0a7569e2164e4d228bb924f7
Rakibul Yeasin <ryeasin03@gmail.com>
2022-02-05 04:09:17 +0600

    Produced More bugs

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const path = require('path');
 const axios = require('axios');
 const sharp = require('sharp');
 const Instagram = require('instagram-web-api')
+const FileCookieStore = require('tough-cookie-filestore2');
 
 // Collect Username and Password from Github's variables as arguments
 const username = process.env.INSTAGRAM_USERNAME;
 const password = process.INSTAGRAM_PASSWORD;
 
-const client = new Instagram({ username, password });
+// Saving Cookies for skipping login next time
+const cookieStore = new FileCookieStore(path.join('./', 'cookies.json'));
+const client = new Instagram({ username, password, cookieStore });
 
 // NASA API
 const api = 'https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo';
@@ -25,10 +28,18 @@ const download = async ({ uri, filename }) => {
     responseType: 'stream'
   });
 
-  let location = path.join('./', 'images', filename);
+  let ext = path.extname(uri);
+  let location = path.resolve(__dirname, 'images', filename + ext);
+  console.log(location);
 
   await res.data.pipe(fs.createWriteStream(location))
-    .on('close', err => console.log(err));
+    .on('close', () => {
+      sharp(location)
+        .resize(1696, 1064)
+        .toBuffer()
+        .then( data => data)
+        .catch( err => console.log(err));
+    });
 };
 
 /**
@@ -45,45 +56,44 @@ const getData = async url => {
   }
 };
 
+
+const post = async ({ data, mTags, credit }) => {
+  // await client.login();
+
+  var photo = data.url;
+  let caption = '';
+
+  if (data.title) caption += data.title + '\n\n';
+  if (data.explanation) caption += data.explanation + '\n\n';
+  if (data.copyright) caption += 'Copyright ' + data.copyright + '\n\n';
+  caption += credit + '\n\n';
+  caption += mTags;
+
+  let ext = path.extname(photo);
+  let location = path.resolve(__dirname, 'images', 'nasa' + ext);
+
+  await download({
+    uri: photo,
+    filename: 'nasa',
+  })
+
+  let { res } = await client.uploadPhoto({
+    photo: location,
+    caption
+  });
+  console.log(Object.keys(res.data.body));
+}
+
 ;(async () => {
   let res = await client.login()
-  console.log(res);
+  // console.log(res);
 
-  getData(api).then(res => {
-  var data = res;
-  let credit = '\n\nThis is an auto-generated and auto-published post.The pictures and captions are taken from the NASA API https://api.nasa.gov/. This System is developed by @drreygur';
-  let mTags = '#astronomy #space #nasa #universe #astrophotography #science #cosmos #moon #stars #galaxy #astrophysics #nightsky #photography #physics #milkyway #spacex #cosmology #astro #earth #astronomia #sky #nature #telescope #astronaut #nightphotography #solarsystem #night #planets #mars #bhfyp';
+  getData(api)
+    .then(async res => {
+      var data = res;
+      let credit = `\n\nThis is an auto-generated and auto-published post.The pictures and captions are taken from the NASA API https://api.nasa.gov/.This System is developed by @drreygur`;
+      let mTags = '#astronomy #space #nasa #universe #astrophotography #science #cosmos #moon #stars #galaxy #astrophysics #nightsky #photography #physics #milkyway #spacex #cosmology #astro #earth #astronomia #sky #nature #telescope #astronaut #nightphotography #solarsystem #night #planets #mars #bhfyp';
 
-  var post = async (data, mTags) => {
-    await client.login();
-
-    var photo = data.url;
-    let caption = '';
-
-    if (data.title) caption += data.title + '\n\n';
-    if (data.explanation) caption += data.explanation + '\n\n';
-    if (data.copyright) caption += 'Copyright ' + data.copyright + '\n\n';
-    caption += credit + '\n\n';
-    caption += mTags;
-
-    download({
-      uri: data.url,
-      filename: 'nasa.jpg',
-    })
-
-    await sharp('./images/nasa.jpg')
-      .resize(1696, 1064)
-      .jpeg({ mozjpeg: true })
-      .toBuffer()
-      .then(async data => {
-          let {res} =  await client.uploadPhoto({
-          photo: './images/nasa.jpg',
-          caption
-        });
-        console.log(res);
-      })
-    .catch( err => console.log(err));
-  }
-  post(data, mTags);
-});
-})()
+      await post({ data, mTags, credit });
+    });
+})();
